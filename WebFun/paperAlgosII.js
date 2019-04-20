cefa7e4fc4c033033a7957a521ef2f0943f000cd
Melissa <mmankt2@gmail.com>
2019-04-20 10:12:47 -0500

    paper algos II commit

--- WebFun/paperAlgosII.js
+++ WebFun/paperAlgosII.js
@@ -84,3 +84,47 @@ function sum(x){
   return sum;
 }
 //nothing
+
+//Part 2
+// 1) Analyze an array’s values and return the average of each respective array.
+function printAverage(x){
+  sum = 0;
+  // your code here
+  var len = x.length;
+  for (var i=0;i<len;i++){
+    sum+=x[i];
+  }
+  var avg = sum/len;
+  return avg;
+}
+y = printAverage([1,2,3]);
+console.log(y); // should log 2
+y = printAverage([5,2,8]);
+console.log(y); // should log 5
+
+//2) Create an array with all the odd integers between 1 and 255 (inclusive)
+function returnOddArray(){
+  // your code here
+  var arr = []
+  for (var i = 1;i<=255;i+=2){
+    arr.push(i);
+  }
+  return arr;
+}
+y = returnOddArray();
+console.log(y); // should log [1,3,5,...,253,255]
+
+//3) Square each value in a given array, returning that same array with changed values.
+function squareValue(x){
+  // your code here
+  var len = x.length;
+  for (var i=0;i<len;i++){
+    x[i]=x[i]*x[i];
+  }
+  return x;
+}
+y = squareValue([1,2,3]);
+console.log(y); // should log [1,4,9]
+ 
+y = squareValue([2,5,8]);
+console.log(y); // should log [4,25,64]
