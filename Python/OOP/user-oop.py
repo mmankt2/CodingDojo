f17073e904f38327ea6604d32213e13bb1fda53d
Melissa <mmankt2@gmail.com>
2019-06-14 17:52:33 -0500

    Users OOP with chaining

--- Python/OOP/user-oop.py
+++ Python/OOP/user-oop.py
@@ -8,10 +8,13 @@ class User:		# here's what we have so far
     # adding the deposit method
   def make_deposit(self, amount):	# takes an argument that is the amount of the deposit
     self.account_balance += amount	# the specific user's account increases by the amount of the value receivedc
+    return self
   def make_withdrawal(self, amount): #taks an argument that is the amount of the withdrawal
     self.account_balance = self.account_balance - amount #the specific user's account decreases by the amount of the withdrawal
+    return self
   def display_user_balance(self): #print the user's name and account balance
     print("{}: ${}".format(self.name,self.account_balance))
+    return self
 
 #create 3 instances of the User classe
 guido = User("Guido van Rossum", "guido@python.com")
@@ -22,15 +25,7 @@ print(guido.name)	# output: Guido van Rossum
 print(monty.name)	# output: Monty Python
 
 #have the first user make 3 deposits and 1 withdrawal. display balance.
-guido.make_deposit(100)
-guido.make_deposit(200)
-guido.make_deposit(300)
-guido.make_withdrawal(50)
-guido.display_user_balance()
+guido.make_deposit(100).make_deposit(200).make_deposit(300).make_withdrawal(50).display_user_balance()
 
 #have the third user make 1 deposit and 3 withdrawals. display balance.
-melissa.make_deposit(100)
-melissa.make_withdrawal(50)
-melissa.make_withdrawal(50)
-melissa.make_withdrawal(50)
-melissa.display_user_balance()
+melissa.make_deposit(100).make_withdrawal(50).make_withdrawal(50).make_withdrawal(50).display_user_balance()
