6e80982903c001b4ca897e4f83fa5e0be10a2dfd
Melissa <mmankt2@gmail.com>
2019-06-14 17:47:57 -0500

    Users OOP

--- Python/OOP/user-oop.py
+++ Python/OOP/user-oop.py
@@ -13,16 +13,24 @@ class User:		# here's what we have so far
   def display_user_balance(self): #print the user's name and account balance
     print("{}: ${}".format(self.name,self.account_balance))
 
+#create 3 instances of the User classe
 guido = User("Guido van Rossum", "guido@python.com")
 monty = User("Monty Python", "monty@python.com")
+melissa = User("Melissa Littleton","mmankt2@gmail.com")
+
 print(guido.name)	# output: Guido van Rossum
 print(monty.name)	# output: Monty Python
 
+#have the first user make 3 deposits and 1 withdrawal. display balance.
 guido.make_deposit(100)
 guido.make_deposit(200)
-monty.make_deposit(50)
-print(guido.account_balance)	# output: 300
-print(monty.account_balance)	# output: 50
-
+guido.make_deposit(300)
 guido.make_withdrawal(50)
 guido.display_user_balance()
+
+#have the third user make 1 deposit and 3 withdrawals. display balance.
+melissa.make_deposit(100)
+melissa.make_withdrawal(50)
+melissa.make_withdrawal(50)
+melissa.make_withdrawal(50)
+melissa.display_user_balance()
