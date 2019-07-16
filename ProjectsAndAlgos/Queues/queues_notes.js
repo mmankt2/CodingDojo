4747ced8e4cf2bceb4b18fb520cc3d4452316b67
Melissa <mmankt2@gmail.com>
2019-07-16 16:39:41 -0500

    minor changes to the black cat

---
+++ ProjectsAndAlgos/Queues/queues_notes.js
@@ -0,0 +1,17 @@
+//create a queue using SList
+function SLNode(value) {
+  this.val = value;
+  this.next = null;
+}
+function SLQueue() {
+  this.head = null;
+  this.tail = null;
+}
+
+//to imprement a circular queue
+function CirQueue(cap) {
+  var head = 0;
+  var tail = 0;
+  var capacity = cap;
+  var arr = [];
+}
